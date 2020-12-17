b64d40efee2e001939eb0df804f67fa6ad791b69
jefDimar <jefdimar@gmail.com>
2020-12-17 20:23:12 +0700

    adding routes for login & register

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router()
 const Controller = require('../controllers/controller')
 const ControllerEvent = require('../controllers/controllerEvent')
+// add controller for User
+const ControllerUser = require('../controllers/controllerUser')
 
 // Index / homepage
 routes.get('/', Controller.showHome)
@@ -9,5 +11,12 @@ routes.get('/', Controller.showHome)
 routes.get('/events', ControllerEvent.showAll)
 routes.get('/events/add', ControllerEvent.formAdd)
 routes.post('/events/add', ControllerEvent.addEvent)
+
+// route for register user
+routes.get('/register', ControllerUser.formRegister)
+routes.post('/register', ControllerUser.addUser)
+// route for login user
+routes.get('/login', ControllerUser.formLogin)
+routes.post('/login', ControllerUser.login)
 
 module.exports = routes
