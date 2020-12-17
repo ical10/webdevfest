89cfff92135045ce9c9c1646f5439f6ba917fdfe
ical10 <mochhusnir@gmail.com>
2020-12-17 21:17:40 +0700

    adding level column on User model

--- migrations/20201216123141-create-user.js
+++ migrations/20201216123141-create-user.js
@@ -11,6 +11,9 @@ module.exports = {
       username: {
         type: Sequelize.STRING
       },
+      level: {
+        type: Sequelize.STRING
+      },
       password: {
         type: Sequelize.STRING
       },
