dc3a7602df5796a8ea8de50bae57b7377909c65b
jefDimar <jefdimar@gmail.com>
2020-12-17 20:25:17 +0700

    add for login & register

---
+++ controllers/controllerUser.js
@@ -0,0 +1,57 @@
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+
+class ControllerUser {
+  static formRegister(req, res) {
+    res.render('formRegister')
+  }
+
+  static addUser(req, res) {
+    const input = {
+      username: req.body.username,
+      password: req.body.password,
+      level: req.body.level,
+      language: req.body.language,
+      role: 'user'
+    }
+
+    User.create(input)
+      .then(_ => {
+        res.redirect('/login')
+      })
+      .catch(err => {
+        res.send(err)
+      })
+  }
+
+  static formLogin(req, res) {
+    res.render('formLogin')
+  }
+
+  static login(req, res) {
+    const input = {
+      username: req.body.username,
+      password: req.body.password
+    }
+
+    User.findOne({ where: { username: input.username } })
+      .then(user => {
+        if (user) {
+          const validate = bcrypt.compareSync(input.password, user.password)
+
+          if (validate) {
+            res.redirect('/events')
+          } else {
+            res.send('Username & Password mismatch')
+          }
+        } else {
+          res.send('Username mismatch')
+        }
+      })
+      .catch(error => {
+        res.send(error)
+      })
+  }
+}
+
+module.exports = ControllerUser
