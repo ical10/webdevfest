2df7d4c40e1e887d070570c7e034b4520f3bcf23
jefDimar <jefdimar@gmail.com>
2020-12-17 22:37:19 +0700

    modify routes to match session & add global session before CRUD Events

--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,11 @@ const Controller = require('../controllers/controller')
 const ControllerEvent = require('../controllers/controllerEvent')
 // add controller for User
 const ControllerUser = require('../controllers/controllerUser')
+// require middlewares
+const { isLoggedIn, isAdmin } = require('../middlewares/authentication')
 
 // Index / homepage
 routes.get('/', Controller.showHome)
-
-// route CR Event
-routes.get('/events', ControllerEvent.showAll)
-routes.get('/events/add', ControllerEvent.formAdd)
-routes.post('/events/add', ControllerEvent.addEvent)
 
 // route for register user
 routes.get('/register', ControllerUser.formRegister)
@@ -18,5 +15,16 @@ routes.post('/register', ControllerUser.addUser)
 // route for login user
 routes.get('/login', ControllerUser.formLogin)
 routes.post('/login', ControllerUser.login)
+
+// session for login first
+routes.use(isLoggedIn)
+
+// route CR Event
+routes.get('/events', ControllerEvent.showAll)
+
+// add session if user role is admin
+routes.get('/events/add', isAdmin, ControllerEvent.formAdd)
+routes.post('/events/add', isAdmin, ControllerEvent.addEvent)
+
 
 module.exports = routes
